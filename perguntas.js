44d4f018e750d1c5784f5777a3115da8749769d2
2Cfadinha31 <169051696+2Cfadinha31@users.noreply.github.com>
2024-11-24 13:35:52 -0300

    Upload PontoCards

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Curiosidades',
+    'Qual o descendente dos porquinhos-da-índia?',
+    'O descendente dos porquinhos-da-índia é o Cavia tschudii.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Há quantas cores de porquinhos-da-india?',
+    'No total há 5 cores, sendo elas, bicolores, tricolores, albinos, amarelados e totalmente pretos.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual o tamanho máximo que um porquinho-da-india pode chegar?',
+    'Os porquinhos-da-india podem medir entre 20 e 25 centímetros'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Quantos anos um porquinha-da-india vive?',
+    'Os porquinhos-da-india podem viver de 4 à 5 anos.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual o continente de origem dos porquinhos-da-india?',
+    'Sua descendência é oriunda da América do Sul, sendo mais comum no Peru.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Há quantas raças de porquinhos-da-india?',
+    'No total há 6 raças de porquinhos-da-india.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual o principal alimentos dos porquinhos-da-india?',
+    'A alimentação desse animal consiste principalmente no consumo de feno.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Onde é possível comprar os porquinhos-da-india?',
+    'O pet é facilmente encontrado em grandes lojas de petshop, mas lembre-se que ao comprar o bixinho, é preciso muito cuidado.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Porque os porquinhos-da-india são pets tímidos e pouco sociáveis?',
+    'O pet por ser uma presa dentro da natureza, o animal tende a ser mais tímido por uma questão de defesa.'
+)
